be8ecd8f33887b43267018603977fedcb5b6455f
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2024-12-18 12:08:50 +0000

    added blog navlink

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 # Display all posts
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts})
 
 # Create a new post
 def post_create(request):
@@ -13,13 +13,13 @@ def post_create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()  # Save the new post to the database
-            return redirect('post_list')  # Redirect to the list of posts after saving
+            return redirect('index.html')  # Redirect to the list of posts after saving
     else:
         form = PostForm()  # If it's a GET request, show an empty form
 
-    return render(request, 'blog/post_form.html', {'form': form})
+    return render(request, 'blog/index.html', {'form': form})
 
 # View a single post
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/index.html', {'post': post})
