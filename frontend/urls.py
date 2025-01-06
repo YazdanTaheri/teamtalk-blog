da1e3cee2742c11cda9c16eb79e6bfa91099a68e
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2025-01-06 15:36:28 +0000

    add the category

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from django.urls import path, include
 from frontend import views
 
+
 urlpatterns = [
     path('', views.category_list, name='home'),
+    path('blog/category/<int:category_id>/', views.posts_by_category, name='posts_by_category'),
+    path('blog/category/<slug:slug>/', views.category_list, name='category_list'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from blog.models import Category
-from django.shortcuts import render
+from blog.models import Category, Post
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
     if request.method =="POST":
@@ -19,3 +19,8 @@ def category_list(request):
         'categories': categories,
     }
     return render(request, 'frontend/index.html', context)
+
+def posts_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    posts = Post.objects.filter(category=category)
+    return render(request, 'blog/posts_by_category.html', {'category': category, 'posts': posts})
