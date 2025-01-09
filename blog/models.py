6148aecd083e4185ec90f44f7de09f39dd4b7381
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2025-01-09 16:59:40 +0000

    buttons and edit and delete restriction to admin only

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name   
