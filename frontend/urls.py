134b793ee71158f1b60a0726e4ff77880542a520
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2025-01-11 19:35:16 +0000

    post details style

--- frontend/urls.py
+++ frontend/urls.py
@@ -8,6 +8,8 @@ from frontend import views
 
 urlpatterns = [
     path('', views.category_list, name='home'),
-    path('blog/category/<int:category_id>/', views.posts_by_category, name='posts_by_category'),
-    path('blog/category/<slug:slug>/', views.category_list, name='category_list'),
+    path('blog/category/<int:category_id>/',
+         views.posts_by_category, name='posts_by_category'),
+    path('blog/category/<slug:slug>/',
+         views.category_list, name='category_list'),
 ]
